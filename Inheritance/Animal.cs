fe17e049a0da30dc4d4fc648ff66403beb6a9830
davidrodriguez88 <davidxrodriguez@me.com>
2025-06-14 14:16:00 -0400

    Completed Exercise and even figured out how to add color!

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,45 @@
+﻿namespace Inheritance;
+
+public class Animal
+{
+    public Animal()
+    {
+
+    }
+    
+    public int Legs { get; set; }
+    public int Eyes { get; set; }
+    public bool Heterotrophic { get; set; }
+    public bool Multicellular { get; set; }
+}
+
+public class Bird : Animal
+{
+    public Bird()
+    {
+        Feathers = true;
+        LayEggs = true;
+        Beak = true;
+        Bipedal = true;
+    }
+    public bool Feathers { get; set; }
+    public bool LayEggs { get; set; }
+    public bool Beak { get; set; }
+    public bool Bipedal { get; set; }
+}
+
+public class Reptile : Animal
+{
+    public Reptile()
+    {
+        ColdBlooded = true;
+        Scales = true;
+        InternalFertilization = true;
+        ThreeOrMoreHeartChambers = true;
+    }
+    public bool ColdBlooded { get; set; }
+    public bool Scales { get; set; }
+    public bool InternalFertilization { get; set; }
+    public bool ThreeOrMoreHeartChambers { get; set; }
+    
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,6 +6,43 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+            var myBird = new Bird();
+            {
+                myBird.Legs = 2;
+                myBird.Eyes = 2;
+                myBird.Heterotrophic = true;
+                myBird.Multicellular = true;
+            }
+            ;
+            var myReptile = new Reptile();
+            {
+                myReptile.Legs = 4;
+                myReptile.Eyes = 2;
+                myReptile.Heterotrophic = true;
+                myReptile.Multicellular = true;
+            }
+            ;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Bird Characteristics:");
+            Console.WriteLine($"Legs: {myBird.Legs}");
+            Console.WriteLine($"Eyes: {myBird.Eyes}");
+            Console.WriteLine($"Has Feathers? {myBird.Feathers}");
+            Console.WriteLine($"Lays Eggs? {myBird.LayEggs}");
+            Console.WriteLine($"Has Beak? {myBird.Beak}");
+            Console.WriteLine($"Bipedal? {myBird.Bipedal}");
+            Console.ResetColor();
+            Console.WriteLine();
+            
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Reptile Characteristics:");
+            Console.WriteLine($"Legs: {myReptile.Legs}");
+            Console.WriteLine($"Eyes: {myReptile.Eyes}");
+            Console.WriteLine($"Cold Blooded? {myReptile.ColdBlooded}");
+            Console.WriteLine($"Has Scales? {myReptile.Scales}");
+            Console.WriteLine($"Fertilizes Internally? {myReptile.InternalFertilization}");
+            Console.WriteLine($"Three or more Heart Chambers? {myReptile.ThreeOrMoreHeartChambers}");
+            
+            
             // TODO Be sure to follow best practice when creating your classes
 
             // Create a class Animal
@@ -21,18 +58,16 @@ namespace Inheritance
             // Set this class to inherit from your Animal Class
 
 
-
-
             /*Create an object of your Bird class
              *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
+             *
+             * Creatively display the class member values
              */
 
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
-             *  
-             * Creatively display the class member values 
+             *
+             * Creatively display the class member values
              */
         }
     }
